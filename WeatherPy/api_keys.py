9554f902ffeeb1cd3767c5cc4d4b124df3fdeb4c
Abigail Janvier <abigailjanvier751@gmail.com>
2023-02-02 00:14:59 -0600

    completed WeatherPy

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "3f4d28449801e9428d69a46414e2897a"
+weather_api_key = '3f4d28449801e9428d69a46414e2897a'
 
 # Geoapify API Key
 geoapify_key = "YOUR KEY HERE"
